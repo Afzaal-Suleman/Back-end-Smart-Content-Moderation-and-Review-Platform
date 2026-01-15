899b782bb8d44008dd0dbf199cd224ac9d3483db
Afzaal Suleman <afzaaladi48@gmail.com>
2026-01-15 06:39:55 +0500

    db

--- api/config/database.ts
+++ api/config/database.ts
@@ -1,23 +1,27 @@
 import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'mydb' as string,
-  process.env.DB_USER || 'postgres' as string,
-  process.env.DB_PASSWORD || 'admin' as string,
+  process.env.DB_NAME || "mydb",
+  process.env.DB_USER || "postgres",
+  process.env.DB_PASSWORD || "admin",
   {
     host: process.env.DB_HOST || "localhost",
     dialect: "postgres",
     logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000,
+    },
   }
 );
 
 export const connectDB = async () => {
   try {
     await sequelize.authenticate();
-    console.log("PostgreSQL connected");
-  } catch (error) {
-    console.error("DB connection failed:", error);
-    process.exit(1);
+    console.log("✅ PostgreSQL connected successfully");
+  } catch (error: any) {
+    console.error("⚠️ DB connection failed:", error.message);
   }
 };
 
